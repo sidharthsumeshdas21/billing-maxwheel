174a31719886fe77e9cffed8ec00553398cfee5c
sidharthsumeshdas21 <sidharthsumeshdas@gmail.com>
2026-06-21 10:38:01 +0530

    fix: add .onrender.com to ALLOWED_HOSTS

--- maxwheel/settings.py
+++ maxwheel/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,.onrender.com', cast=Csv())
 
 INSTALLED_APPS = [
     'django.contrib.admin',
